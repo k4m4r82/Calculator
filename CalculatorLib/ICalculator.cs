b378c52de3f9de5ecfa7ea0cf4a0bfd2c092ebaf
k4m4r82 <k4m4r82@gmail.com>
2015-05-02 11:02:46 +0700

    First commit

---
+++ CalculatorLib/ICalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ServiceModel;
+
+namespace CalculatorLib
+{
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract]
+        float Penambahan(float a, float b);
+
+        [OperationContract]
+        float Pengurangan(float a, float b);
+
+        [OperationContract]
+        float Perkalian(float a, float b);
+
+        [OperationContract]
+        float Pembagian(float a, float b);
+    }
+}
