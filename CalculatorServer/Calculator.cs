7f686fc902bde7c36496b84d34057cc513c2a9d4
k4m4r82 <k4m4r82@gmail.com>
2015-05-09 00:14:08 +0700

    Penambahan project CalculatorServer

---
+++ CalculatorServer/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// namespace CalculatorLib perlu ditambahkan
+// agar bisa mengakses interface ICalculator
+using CalculatorLib;
+
+using System.ServiceModel;
+
+namespace CalculatorServer
+{
+    // class Calculator mengimplementasikan interface ICalculator
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Single, InstanceContextMode = InstanceContextMode.PerCall)]
+    public class Calculator : ICalculator
+    {
+        public float Penambahan(float a, float b)
+        {
+            return a + b;
+        }
+
+        public float Pengurangan(float a, float b)
+        {
+            return a - b;
+        }
+
+        public float Perkalian(float a, float b)
+        {
+            return a * b;
+        }
+
+        public float Pembagian(float a, float b)
+        {
+            return a / b;
+        }
+    }
+}
